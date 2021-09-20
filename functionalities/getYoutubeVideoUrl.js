e4e455ee1d015eeb7a575a7323fe2084276572d1
Caio Costa <caiocrrodrigues2004@gmail.com>
2021-09-20 01:07:37 -0300

    Alteração no serviço de pesquisa no Youtube
    Agora o "getYoutubeVideoUrl.js" não usa mais a API da Google para fazer pesquisas no Youtube, mas sim uma library que não necessita de uma key para usar e não há limite de requisições (sem cotas).

--- functionalities/getYoutubeVideoUrl.js
+++ functionalities/getYoutubeVideoUrl.js
@@ -1,16 +1,32 @@
 const config = require("../config.json");
 const google = require("googleapis");
+const yt = require("youtube-search-without-api-key");
 
 const youtube = new google.youtube_v3.Youtube({
     version: "v3",
     auth: config.GOOGLE_KEY
 });
 
-module.exports = (input, channel, callback) => {
+module.exports = async (input, channel, callback) => {
+    try {
+        const videos = await yt.search(input);
+        // Pegando o primeiro objeto do Array (primeiro resultado da pesquisa no Youtube)
+        let videoId = videos[0].id.videoId;
+        let link = `https://www.youtube.com/watch?v=${videoId}`;
+        callback(link);
+    } catch (err) {
+        channel.send(
+            "Um erro foi encontrado ao tentar tocar a música...\n" +
+            "```"+err.message+"```"
+        )
+    }
+
     /*
      * API da Google que fornece o ID do vídeo no Youtube, para então 
      * conseguir dar play através da concatenação no URL
-    */ 
+    */
+
+    /*
     youtube.search.list({
         q: input,
         part: "snippet",
@@ -34,10 +50,11 @@ module.exports = (input, channel, callback) => {
     
         else if (resultado) {
             let videoId = resultado.data.items[0].id.videoId;
-            /*let title = resultado.data.items[0].snippet.title*/;
+            //let title = resultado.data.items[0].snippet.title
 
             let link = `https://www.youtube.com/watch?v=${videoId}`;
             callback(link);
         }
     });
+    */
 }
