e47b7eb292250d841020b65c924b650dcc322251
Caio Costa <caiocrrodrigues2004@gmail.com>
2021-09-21 21:05:14 -0300

    Pasta "dist" removida

--- dist/commands/join.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.join = void 0;
-const index_1 = require("../index");
-function join(msg) {
-    (0, index_1.assignConnection)(msg);
-}
-exports.join = join;
